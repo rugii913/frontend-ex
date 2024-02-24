2d6fe98bd13cf5f93efc274764df52cb9fcfa618
rugii913 <rugii913@gmail.com>
2024-02-25 00:47:56 +0900

    Refactor: 명확성을 위해 js 확장자를 jsx로 변경
    - 명확성을 위해 js 확장자를 jsx로 변경
      - 확장자(extension)이 js여도 동작에는 문제가 없음
      - 하지만 조금 더 명확하게 보여주려고 함
    
    - (cf.) index.js도 jsx로 바꾸려고 했으나 실패함
      - 단순하게 index.js만 jsx로 바꾸면 동작하지 않음
      - 아마도 node_modules 디렉토리의 파일들을 건드려야하지 않을까 추측함
      - (cf.) CRA 공식 문서 https://create-react-app.dev/

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function Header() {
     <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
       <p>
-        Edit <code>src/App.js</code> and save to reload.
+        Edit <code>src/App.jsx</code> and save to reload.
       </p>
       <a
         className="App-link"
