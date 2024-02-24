1edeada34c6f5a58a3183c129fe445dc4807b6d0
rugii913 <rugii913@gmail.com>
2024-02-25 02:59:08 +0900

    Feat: Props를 이용한 컴포넌트 재사용 1
    - Props를 이용한 컴포넌트 재사용

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import logo from './logo.svg';
 //   - 순수 JS를 사용한다면 이런 것은 불가능함
 // - 아무튼 React에서는 이런 방식으로 이미지를 변수처럼 받아서 코드에서 활용할 수 있음
 import './App.css'; // 위와 마찬가지로 순수 JS에서는 CSS 파일을 이런 식으로 가져오지 못함
+import componentsImg from './assets/components.png'
 
 const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
@@ -37,10 +38,35 @@ function Header() {
   );
 }
 
+function CoreConcept(props) {
+  return (
+    <li>
+      <img src={props.image} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
+}
+
 function App() {
   return (
     <div className="App">
       <Header />
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept
+              title="Components"
+              description="The core UI building block."
+              image={componentsImg}
+            />
+            <CoreConcept title="Props" />
+            <CoreConcept />
+            <CoreConcept />
+          </ul>
+        </section>
+      </main>
     </div>
   );
 }
