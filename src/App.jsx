79a4a1a182b084e7da413bb99febe01347f7ca5c
rugii913 <rugii913@gmail.com>
2024-02-25 01:19:46 +0900

    Add: {} 사용, jsx에서 동적 값 활용하기
    - {}을 사용하여 jsx에서 동적 값 활용하기
    - (cf.) if문, for문, 함수 정의, 블록문은 {} 안에 넣을 수 없다
      - value를 만들어내는 식(expression)만 {} 안에 넣을 수 있음

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
+
+function generateRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
 function Header() {
+  const description = reactDescriptions[generateRandomInt(2)];
+
   return (
     <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
@@ -16,6 +24,9 @@ function Header() {
       >
         Learn React
       </a>
+      <p>
+        { description } React concepts you will need for almost any app you are going to build!
+      </p>
     </header>
   );
 }
