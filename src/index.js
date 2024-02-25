ccb20fc2cfd6bfa900f8950e80e21d7f413dc134
rugii913 <rugii913@gmail.com>
2024-02-26 01:09:53 +0900

    refactor: JSX를 사용하지 않는 방법
    - -JSX를 사용하지 않는 방법
      - createElement()와 순수 JS만 사용하여 JSX를 사용하지 않을 수도 있다.

--- src/index.js
+++ src/index.js
@@ -14,7 +14,8 @@ import reportWebVitals from './reportWebVitals';
 
 // 강의 스타일 - Vite 자동 생성 스타일로 추청
 const entryPoint = document.getElementById("root");
-ReactDOM.createRoot(entryPoint).render(<App />);
+// ReactDOM.createRoot(entryPoint).render(<App />);
+ReactDOM.createRoot(entryPoint).render(React.createElement(App));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
