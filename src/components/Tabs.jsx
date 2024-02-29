b9a342b954676479e1799a541dae21a9ecb74af1
rugii913 <rugii913@gmail.com>
2024-03-01 02:24:13 +0900

    refactor: JS 문법 사용하여 prop 기본값 설정하기
    - JS 문법 사용하여 prop 기본값 설정하기

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,4 +1,4 @@
-export default function Tabs({ children, buttons, ButtonsContainer }) {
+export default function Tabs({ children, buttons, ButtonsContainer ="menu" }) {
     // const ButtonsContainer = buttonsContainer; // 이 변수명은 대문자로 시작해야 함
 
     return <>
