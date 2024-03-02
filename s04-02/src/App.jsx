ed590582fb8b7ceadd3646969a3d576ee9a4f532
rugii913 <rugii913@gmail.com>
2024-03-03 00:21:43 +0900

    refactor: 컴포넌트 간의 state 공유
    - 컴포넌트 간의 state 공유
      - App의 gameTurns 상태 하나를 이용하여, Log, GameBoard 두 컴포넌트를 모두 업데이트 하는 구조를 완성함

--- s04-02/src/App.jsx
+++ s04-02/src/App.jsx
@@ -39,7 +39,7 @@ function App() {
           turns={gameTurns}
         />
       </div>
-      <Log />
+      <Log turns={gameTurns}/>
     </main>
   );
 }
--- s04-02/src/components/Log.jsx
+++ s04-02/src/components/Log.jsx
@@ -1,5 +1,12 @@
-export default function Log() {
-
-  return <ol id="log">
-  </ol>;
+export default function Log({ turns }) {
+  return (
+    <ol id="log">
+      {turns.map((turn) => (
+        <li key={`${turn.square.row}${turn.square.col}`}>
+          {turn.player} selected {turn.square.row},{turn.square.col}
+        </li>
+      ))}
+      {/* 백틱 `` 사용하여 JS 템플릿 리터럴 사용 */}
+    </ol>
+  );
 }
