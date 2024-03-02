b4459e863a78f176747c6c6d5348400c307d2852
rugii913 <rugii913@gmail.com>
2024-03-03 00:14:10 +0900

    refactor: props에서 state 파생하기
    - props에서 state 파생하기

--- s04-02/src/App.jsx
+++ s04-02/src/App.jsx
@@ -34,7 +34,10 @@ function App() {
           <Player initialName="Player 1" symbol="X" isActive={activePlayer === "X"} />
           <Player initialName="Player 2" symbol="O" isActive={activePlayer === "O"} />
         </ol>
-        <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer} />
+        <GameBoard
+          onSelectSquare={handleSelectSquare} 
+          turns={gameTurns}
+        />
       </div>
       <Log />
     </main>
--- s04-02/src/components/GameBoard.jsx
+++ s04-02/src/components/GameBoard.jsx
@@ -4,7 +4,16 @@ const initialGameBoard = [
   [null, null, null],
 ]; // 상태가 아니므로 함수 밖에 둔다. (계속 관리해줘야하는 변수가 아니라 처음에 한 번 주면 충분한 값임)
 
-export default function GameBoard({ onSelectSquare }) {
+export default function GameBoard({ onSelectSquare, turns }) {
+  let gameBoard = initialGameBoard;
+
+  for (const turn of turns) {
+    const { square, player } = turn;
+    const { row, col } = square;
+
+    gameBoard[row][col] = player;
+  }
+
   // const [gameBoard, setGameBoard] = useState(initialGameBoard); // 이 GameBoard 컴포넌트에서는 상태를 제어하지 않게 할 것
 
   // function handleSelectSquare(rowIndex, colIndex) {
@@ -31,7 +40,7 @@ export default function GameBoard({ onSelectSquare }) {
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={onSelectSquare}>
+                <button onClick={() => onSelectSquare(rowIndex, colIndex)}>
                   {playerSymbol}
                 </button>
               </li>
