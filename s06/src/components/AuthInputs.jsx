3b558bba580a8108dd76e1fb5413529f3799a386
rugii913 <rugii913@gmail.com>
2024-03-03 22:42:44 +0900

    (s6)feat:styled components로 스타일이 적용된 유동적 컴포넌트 생성
    - styled components로 스타일이 적용된 유동적 컴포넌트 생성

--- s06/src/components/AuthInputs.jsx
+++ s06/src/components/AuthInputs.jsx
@@ -14,6 +14,27 @@ const ControlContainer = styled.div`
 - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals#tagged_templates
 */
 
+const Label = styled.label`
+  display: block;
+  margin-bottom: 0.5rem;
+  font-size: 0.75rem;
+  font-weight: 700;
+  letter-spacing: 0.1em;
+  text-transform: uppercase;
+  color: #6b7280;
+`;
+
+const Input = styled.input`
+  width: 100%;
+  padding: 0.75rem 1rem;
+  line-height: 1.5;
+  background-color: #d1d5db;
+  color: #374151;
+  border: 1px solid transparent;
+  border-radius: 0.25rem;
+  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
+`;
+
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState('');
   const [enteredPassword, setEnteredPassword] = useState('');
@@ -38,8 +59,8 @@ export default function AuthInputs() {
     <div id="auth-inputs">
       <ControlContainer>
         <p>
-          <label className={`label ${emailNotValid ? "invalid" : ""}`}>Email</label>
-          <input
+          <Label className={`label ${emailNotValid ? "invalid" : ""}`}>Email</Label>
+          <Input
             type="email"
             // style={{
             //   backgroundColor: emailNotValid ? "#fed2d2" : "#d1d5db"
@@ -49,8 +70,8 @@ export default function AuthInputs() {
           />
         </p>
         <p>
-          <label className={`label ${passwordNotValid ? "invalid" : ""}`}>Password</label>
-          <input
+          <Label className={`label ${passwordNotValid ? "invalid" : ""}`}>Password</Label>
+          <Input
             type="password"
             className={passwordNotValid ? 'invalid' : undefined}
             onChange={(event) =>
