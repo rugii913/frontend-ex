1e2831ac08eddd0f312f534487d85d16d3408dcd
rugii913 <rugii913@gmail.com>
2024-03-04 00:57:24 +0900

    (s6)refactor:(??미디어쿼리)styled components 중첩 규칙, 미디어 쿼리, 가상 선택자
    - styled components의 여러 활용 방법
      - 중첩 규칙
      - 미디어 쿼리
      - 가상 선택자
    - (cf.) 미디어 쿼리가 뭔지 더 찾아볼 것

--- s06/src/components/AuthInputs.jsx
+++ s06/src/components/AuthInputs.jsx
@@ -36,6 +36,21 @@ const Input = styled.input`
   box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
 `;
 
+const Button = styled.button`
+  padding: 1rem 2rem;
+  font-weight: 600;
+  text-transform: uppercase;
+  border-radius: 0.25rem;
+  color: #1f2937;
+  background-color: #f0b322;
+  border-radius: 6px;
+  border: none;
+
+  &:hover {
+    background-color: #f0920e;
+  }
+`;
+
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState('');
   const [enteredPassword, setEnteredPassword] = useState('');
@@ -64,7 +79,7 @@ export default function AuthInputs() {
           <Input
             type="email"
             $invalid={emailNotValid}
-            onChange={(event) => handleInputChange('email', event.target.value)}
+            onChange={(event) => handleInputChange("email", event.target.value)}
           />
         </p>
         <p>
@@ -73,7 +88,7 @@ export default function AuthInputs() {
             type="password"
             $invalid={passwordNotValid}
             onChange={(event) =>
-              handleInputChange('password', event.target.value)
+              handleInputChange("password", event.target.value)
             }
           />
         </p>
@@ -82,7 +97,7 @@ export default function AuthInputs() {
         <button type="button" className="text-button">
           Create a new account
         </button>
-        <button className='button' onClick={handleLogin}>Sign In</button>
+        <Button onClick={handleLogin}>Sign In</Button>
       </div>
     </div>
   );
--- s06/src/components/Header.jsx
+++ s06/src/components/Header.jsx
@@ -1,11 +1,54 @@
+import { styled } from  "styled-components";
+
 import logo from '../assets/logo.png';
 // import './Header.css';
-import classes from './Header.module.css';
+// import classes from './Header.module.css';
 /* 
 - CSS 모듈을 사용했을 때
   - 장점: jsx 코드와 CSS 코드 분리, 협업 시 유리, CSS가 영향을 미치는 위치를 해당 CSS 파일을 직접 임포트하는 컴포넌트로 스코핑 가능
   - 단점: CSS를 꼭 알아야만 한다, 작고 많은 CSS 파일들을 갖게 될 가능성이 높다.
 */
+
+const StyledHeader = styled.header`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-top: 2rem;
+  margin-bottom: 2rem;
+
+  & img {
+    object-fit: contain;
+    margin-bottom: 2rem;
+    width: 11rem;
+    height: 11rem;
+  }
+
+  & h1 {
+    font-size: 1.5rem;
+    font-weight: 600;
+    letter-spacing: 0.4em;
+    text-align: center;
+    text-transform: uppercase;
+    color: #9a3412;
+    font-family: "Pacifico", cursive;
+    margin: 0;
+  }
+
+  & p {
+    text-align: center;
+    color: #a39191;
+    margin: 0;
+  }
+
+  @media (min-width: 768px) {
+    margin-bottom: 4rem;
+
+    & h1 {
+      font-size: 2.25rem;
+    }
+  }
+`; // &는 해당 컴포넌트의 내부 요소 혹은 이미지 등에 적용됨을 표시해준다.
 
 export default function Header() {
   /* 
@@ -14,10 +57,10 @@ export default function Header() {
     - 단점: 모든 요소를 개별적으로 스타일링해야함, CSS 코드가 JSX 코드로 안으로 들어간다 - 협업 시 불편
   */
   return (
-    <header>
+    <StyledHeader>
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
-      <p className={classes.paragraph}>A community of artists and art-lovers.</p>
+      <p /*className={classes.paragraph}*/>A community of artists and art-lovers.</p>
       {/* 
       <p // style={{
         // color: "red",
@@ -31,6 +74,6 @@ export default function Header() {
      {}으로 JS 표현임을 알리고, {color: "red"} 이런 식으로 JS 객체를 넘긴 것임
      {{}} 자체가 특별한 리액트 기능인 것은 아님
      */}
-    </header>
+    </StyledHeader>
   );
 }
