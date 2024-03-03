3a290fb154da0784971006a114a61df2b642474d
rugii913 <rugii913@gmail.com>
2024-03-04 01:12:17 +0900

    (s6)refactor:재사용 가능한 컴포넌트들을 조합하며 외부로 분리할 때 스타일드 컴포넌트 사용
    - 재사용 가능한 컴포넌트들을 조합하며 외부로 분리할 때 스타일드 컴포넌트 사용

--- s06/src/components/AuthInputs.jsx
+++ s06/src/components/AuthInputs.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { styled } from "styled-components";
 
 import Button from './Button';
+import Input from './Input';
 
 const ControlContainer = styled.div`
   display: flex;
@@ -15,28 +16,6 @@ const ControlContainer = styled.div`
 - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
 - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals#tagged_templates
 */
-
-const Label = styled.label`
-  display: block;
-  margin-bottom: 0.5rem;
-  font-size: 0.75rem;
-  font-weight: 700;
-  letter-spacing: 0.1em;
-  text-transform: uppercase;
-  color: ${({$invalid}) => $invalid ? "#f87171" : "#6b7280"};
-`;
-// 위 동적인 스타일링은 JS tagged template과도 연관됨
-
-const Input = styled.input`
-  width: 100%;
-  padding: 0.75rem 1rem;
-  line-height: 1.5;
-  background-color: ${({$invalid}) => $invalid ? "#fed2d2" : "#d1d5db"};
-  color: ${({$invalid}) => $invalid ? "#ef4444" : "#374151"};
-  border: 1px solid ${({$invalid}) => $invalid ? "#f73f3f" : "transparent"};
-  border-radius: 0.25rem;
-  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
-`;
 
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState('');
@@ -61,24 +40,20 @@ export default function AuthInputs() {
   return (
     <div id="auth-inputs">
       <ControlContainer>
-        <p>
-          <Label $invalid={emailNotValid}>Email</Label>
-          <Input
-            type="email"
-            $invalid={emailNotValid}
-            onChange={(event) => handleInputChange("email", event.target.value)}
-          />
-        </p>
-        <p>
-          <Label $invalid={passwordNotValid}>Password</Label>
-          <Input
-            type="password"
-            $invalid={passwordNotValid}
-            onChange={(event) =>
-              handleInputChange("password", event.target.value)
-            }
-          />
-        </p>
+        <Input
+          label="Email"
+          type="email"
+          invalid={emailNotValid}
+          onChange={(event) => handleInputChange("email", event.target.value)}
+        />
+        <Input
+          label="Password"
+          type="password"
+          invalid={passwordNotValid}
+          onChange={(event) =>
+            handleInputChange("password", event.target.value)
+          }
+        />
       </ControlContainer>
       <div className="actions">
         <button type="button" className="text-button">
