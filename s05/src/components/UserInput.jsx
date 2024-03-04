1c4a9cc817c6b641549cd474faa0db0350302653
rugii913 <rugii913@gmail.com>
2024-03-04 20:59:10 +0900

    (s5)feat:이벤트 핸들링+양방향 바인딩 활용
    - 이벤트 핸들링
    - 양방향 바인딩 활용

--- s05/src/components/UserInput.jsx
+++ s05/src/components/UserInput.jsx
@@ -1,24 +1,72 @@
+import { useState } from "react";
+
 export default function UserInput() {
+  // 각 input 필드에 대해 상태를 따로 관리할지 하나의 객체로 관리할지는 선택 문제
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 1000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10
+  });
+
+  // 각각의 input마다 연결된 네 개의 함수를 만들 수도 있고, 모든 input과 연결되는 함수 한 개를 만들 수도 있다.
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput, // JS 객체를 스프레드
+        [inputIdentifier]: newValue, // inputIdentifier에 해당되는 값만 바꿔치기
+      };
+    });
+  }
+
   return (
     <section id="user-input">
       <div className="input-group">
         <p>
           <label>Initial Investment</label>
-          <input type="number" required />
+          <input
+            type="number"
+            required
+            value={userInput.initialInvestment}
+            onChange={(event) =>
+              handleChange("initialInvestment", event.target.value)
+            }
+          />
         </p>
         <p>
           <label>Annual Investment</label>
-          <input type="number" required />
+          <input
+            type="number"
+            required
+            value={userInput.annualInvestment}
+            onChange={(event) =>
+              handleChange("annualInvestment", event.target.value)
+            }
+          />
         </p>
       </div>
       <div className="input-group">
         <p>
           <label>Initial Expected Return</label>
-          <input type="number" required />
+          <input
+            type="number"
+            required
+            value={userInput.expectedReturn}
+            onChange={(event) =>
+              handleChange("expectedReturn", event.target.value)
+            }
+          />
         </p>
         <p>
           <label>Duration</label>
-          <input type="number" required />
+          <input
+            type="number"
+            required
+            value={userInput.duration}
+            onChange={(event) =>
+              handleChange("duration", event.target.value)
+            }
+          />
         </p>
       </div>
     </section>
