57091a9fd44bf5b7de3adde2af21bdccfd8a16fc
rugii913 <rugii913@gmail.com>
2024-03-05 17:37:22 +0900

    (s6)feat: tailwind에서 미디어 쿼리, 가상 선택자 처리 방법
    - tailwind에서 반응형 페이지를 위한 미디어 쿼리(@media)
      - ex. md:...
    
    - tailwind에서 가상 선택자 처리 방법
      - ex. hover:...

--- s06/src/components/Header.jsx
+++ s06/src/components/Header.jsx
@@ -7,9 +7,15 @@ export default function Header() {
     - 단점: 모든 요소를 개별적으로 스타일링해야함, CSS 코드가 JSX 코드로 안으로 들어간다 - 협업 시 불편
   */
   return (
-    <header className="flex flex-col items-center mt-8 mb-16">
-      <img src={logo} alt="A canvas" className="mb-8 w-44 h-44 object-contain" />
-      <h1 className="text-4xl font-semibold tracking-widest text-center uppercase text-amber-800 font-title">ReactArt</h1>
+    <header className="flex flex-col items-center mt-8 mb-8 md:mb-16">
+      <img
+        src={logo}
+        alt="A canvas"
+        className="mb-8 w-44 h-44 object-contain"
+      />
+      <h1 className="text-xl md:text-4xl font-semibold tracking-widest text-center uppercase text-amber-800 font-title">
+        ReactArt
+      </h1>
       <p className="text-stone-500">A community of artists and art-lovers.</p>
     </header>
   );
