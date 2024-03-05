f1a49c4425427f42d698f51e980a50c9a1172e18
rugii913 <rugii913@gmail.com>
2024-03-05 15:38:55 +0900

    (s10)refactor: context를 소비하는 다른 방법-Consumer 사용
    - context를 소비하는 다른 방법
      - Consumer 사용

--- s10/src/components/Cart.jsx
+++ s10/src/components/Cart.jsx
@@ -1,47 +1,57 @@
-import { useContext } from "react";
+// import { useContext } from "react";
 import { CartContext } from "../store/shopping-cart-context.jsx";
 
 export default function Cart({ onUpdateItemQuantity }) {
   // const cartCtx = useContext(CartContext);
-  const { items } = useContext(CartContext); // 구조분해 가능
-
-  const totalPrice = items.reduce(
-    (acc, item) => acc + item.price * item.quantity,
-    0
-  );
-  const formattedTotalPrice = `$${totalPrice.toFixed(2)}`;
+  // const { items } = useContext(CartContext); // 구조분해 가능
 
   return (
-    <div id="cart">
-      {items.length === 0 && <p>No items in cart!</p>}
-      {items.length > 0 && (
-        <ul id="cart-items">
-          {items.map((item) => {
-            const formattedPrice = `$${item.price.toFixed(2)}`;
+    <CartContext.Consumer>
+      {(cartCtx) => {
+        const totalPrice = cartCtx.items.reduce(
+          (acc, item) => acc + item.price * item.quantity,
+          0
+        );
+        const formattedTotalPrice = `$${totalPrice.toFixed(2)}`;
+        // 함수를 만들어 놓으면 알아서 context value를 받음
+        return (
+          <div id="cart">
+            {cartCtx.items.length === 0 && <p>No items in cart!</p>}
+            {cartCtx.items.length > 0 && (
+              <ul id="cart-items">
+                {cartCtx.items.map((item) => {
+                  const formattedPrice = `$${item.price.toFixed(2)}`;
 
-            return (
-              <li key={item.id}>
-                <div>
-                  <span>{item.name}</span>
-                  <span> ({formattedPrice})</span>
-                </div>
-                <div className="cart-item-actions">
-                  <button onClick={() => onUpdateItemQuantity(item.id, -1)}>
-                    -
-                  </button>
-                  <span>{item.quantity}</span>
-                  <button onClick={() => onUpdateItemQuantity(item.id, 1)}>
-                    +
-                  </button>
-                </div>
-              </li>
-            );
-          })}
-        </ul>
-      )}
-      <p id="cart-total-price">
-        Cart Total: <strong>{formattedTotalPrice}</strong>
-      </p>
-    </div>
+                  return (
+                    <li key={item.id}>
+                      <div>
+                        <span>{item.name}</span>
+                        <span> ({formattedPrice})</span>
+                      </div>
+                      <div className="cart-item-actions">
+                        <button
+                          onClick={() => onUpdateItemQuantity(item.id, -1)}
+                        >
+                          -
+                        </button>
+                        <span>{item.quantity}</span>
+                        <button
+                          onClick={() => onUpdateItemQuantity(item.id, 1)}
+                        >
+                          +
+                        </button>
+                      </div>
+                    </li>
+                  );
+                })}
+              </ul>
+            )}
+            <p id="cart-total-price">
+              Cart Total: <strong>{formattedTotalPrice}</strong>
+            </p>
+          </div>
+        );
+      }}
+    </CartContext.Consumer>
   );
 }
