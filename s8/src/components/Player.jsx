f9b62529d1dace444944c758d17f475fab1690e7
rugii913 <rugii913@gmail.com>
2024-03-12 13:32:58 +0900

    (s8)refactor: refs를 통한 HTML 요소 연결 및 접근 - 전 코드와 비교
    - refs를 통한 HTML 요소 연결 및 접근
      - 이전 코드와 비교 및 동작 비교하면서 볼 것

--- s8/src/components/Player.jsx
+++ s8/src/components/Player.jsx
@@ -1,23 +1,26 @@
+import { useRef } from "react";
 import { useState } from "react";
 
 export default function Player() {
-  const [enteredPlayerName, setEnteredPlayerName] = useState("");
-  const [submitted, setSubmitted] = useState(false);
+  const playerName = useRef();
 
-  function handleChange(event) {
-    setSubmitted(false);
-    setEnteredPlayerName(event.target.value);
-  }
+  const [enteredPlayerName, setEnteredPlayerName] = useState("");
 
   function handleClick() {
-    setSubmitted(true);
+    // - ref로 지정된 변수를 통해 input 컴포넌트에 접근이 가능해짐
+    //   - 그 ref JS 객체는 current 프로퍼티 하나만 갖고 있는 객체임
+    //   - 그리고 그 current 프로퍼티가 연결된 컴포넌트를 가리켜줌
+    setEnteredPlayerName(playerName.current.value);
   }
 
   return (
     <section id="player">
-      <h2>Welcome {submitted ? enteredPlayerName : "unknown entity"}</h2>
+      <h2>Welcome {enteredPlayerName || "unknown entity"}</h2> {/* JS 문법 - ||는 앞 값 truthy이면 앞 값 리턴, falsy이면 뒤 값 리턴 */} {/* (cf.) ||가 아니라 ??은 falsy가 아니라 nullish를 따진다. https://thefirstperson.tistory.com/177 */}
       <p>
-        <input type="text" onChange={handleChange} value={enteredPlayerName} />
+        <input
+          ref={playerName}
+          type="text"
+        />
         <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
