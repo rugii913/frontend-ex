c46ba1f3240b1c3b6ea84c76a617d44cb6cc7427
rugii913 <rugii913@gmail.com>
2024-03-12 15:51:25 +0900

    (s8)feat: 타이머 멈추기-refs를 DOM 요소 제어 외 활용하는 다른 사용 사례
    - 타이머 멈추기
    - refs를 DOM 요소 제어 외 활용하는 다른 사용 사례

--- s8/src/components/TimerChallenge.jsx
+++ s8/src/components/TimerChallenge.jsx
@@ -1,12 +1,17 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
-let timer;
+// let timer;
 // - let 변수인 타이머를 함수 컴포넌트 밖에 선언했을 때의 문제점
 //   - 모든 컴포넌트 인스턴스들이 공유한다.
 //   - 한 타이머가 실행된 상태에서 다른 도전 타이머를 또 실행시키면 원래 타이머가 버려진다.
 // - component 바깥에 let 변수를 선언하면 안 되는 이유
 
 export default function TimerChallenge({ title, targetTime }) {
+  const timer = useRef();
+  // - 컴포넌트 함수 안에 있으므로 특정 컴포넌트 인스턴스에만 할당되며,
+  // - component가 재실행될 때도 같은 주소를 그대로 바라본다.(리액트가 알아서 해결해주는 부분)
+  //   → **참조의 또다른 특별한 기능**: refs는 state와 마찬가지로 component 재실행 시 유실되지 않는다
+
   const [timerStarted, setTimerStarted] = useState(false);
   const [timerExpired, setTimerExpired] = useState(false);
 
@@ -16,7 +21,7 @@ export default function TimerChallenge({ title, targetTime }) {
   //   - handleStart()의 타이머와 handleStop()의 타이머가 서로 다른 타이머가 된다.
 
   function handleStart() {
-    timer = setTimeout(() => {
+    timer.current = setTimeout(() => {
       setTimerExpired(true);
     }, targetTime * 1000);
 
@@ -24,7 +29,7 @@ export default function TimerChallenge({ title, targetTime }) {
   }
 
   function handleStop() {
-    clearTimeout(timer);
+    clearTimeout(timer.current);
   }
 
   return (
