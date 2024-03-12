187d42b839d17eecf9de86fbe385da1748d207dd
rugii913 <rugii913@gmail.com>
2024-03-12 16:28:44 +0900

    (s8)refactor: forwardRef()를 이용한 커스텀 컴포넌트 간 refs 전달
    - forwardRef()를 이용한 커스텀 컴포넌트 간 refs 전달

--- s8/src/components/ResultModal.jsx
+++ s8/src/components/ResultModal.jsx
@@ -1,6 +1,13 @@
-export default function ResultModal({ ref, result, targetTime }) {
+import { forwardRef } from "react";
+
+const ResultModal = forwardRef(function ResultModal( /* forwardRef() 메서드에 prop 부분 수정한 원래의 컴포넌트를 원래 컴포넌트 이름으로 export  */
+  { result, targetTime },
+  ref
+) {
   return (
-    <dialog ref={ref} className="result-modal"> {/* 모든 내장 컴포넌트는 ref 속성을 갖고 있다 - 커스텀 컴포넌트에는 알아서 적절한 변수명으로 넘기면 됨 */}
+    <dialog ref={ref} className="result-modal">
+      {" "}
+      {/* 모든 내장 컴포넌트는 ref 속성을 갖고 있다 - 커스텀 컴포넌트끼리 주고 받을 때는 forwardRef를 사용해야만 한다 */}
       {/* (cf.) dialog는 HTML 기본 지원 태그, dialog는 open 속성이 없으면 안 보임, 그런데 open으로 둘 경우 흐린 배경(backdrop)이 안 나타남 */}
       <h2>You {result}</h2>
       <p>
@@ -14,4 +21,8 @@ export default function ResultModal({ ref, result, targetTime }) {
       </form>
     </dialog>
   );
-}
+});
+
+export default ResultModal;
+/* export default const ResultModal = forwardRef(..) 로는 export 불가능함 */
+/* export default forwardRef(..) 로는 export 가능하긴 함*/
