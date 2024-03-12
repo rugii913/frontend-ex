222e90ea1d57c2e3d105f56d51b97d9a7eaf3469
rugii913 <rugii913@gmail.com>
2024-03-12 14:59:31 +0900

    (s8) chore: 전전 커밋 상태로 코드 복구
    - 전전 커밋 상태로 코드 복구

--- s8/src/components/Player.jsx
+++ s8/src/components/Player.jsx
@@ -4,13 +4,13 @@ import { useState } from "react";
 export default function Player() {
   const playerName = useRef();
 
-  // const [enteredPlayerName, setEnteredPlayerName] = useState("");
+  const [enteredPlayerName, setEnteredPlayerName] = useState("");
 
   function handleClick() {
     // - ref로 지정된 변수를 통해 input 컴포넌트에 접근이 가능해짐
     //   - 그 ref JS 객체는 current 프로퍼티 하나만 갖고 있는 객체임
     //   - 그리고 그 current 프로퍼티가 연결된 컴포넌트를 가리켜줌
-    // setEnteredPlayerName(playerName.current.value); /* DOM에서 바꾸는 게 아니라 읽어들이기만 하므로 괜찮다. */
+    setEnteredPlayerName(playerName.current.value); /* DOM에서 바꾸는 게 아니라 읽어들이기만 하므로 괜찮다. */
     playerName.current.value = ""; /* 선언적이지 않은 명령형 코드이므로 좋지 바람직하지 않다. - 브라우저의 DOM을 입력된 코드로 직접 제어하려 하고 있음  */
     // DOM과의 상호작용은 원칙적으로 사용자가 작성한 명령형 코드가 아니라 리액트가 하게끔 해야 한다.
     // 하지만 최종적으로 많은 코드를 줄여준다면 사용할 수도 있다.
@@ -18,7 +18,7 @@ export default function Player() {
 
   return (
     <section id="player">
-      <h2>Welcome {playerName.current ? playerName.current.value : "unknown entity"}</h2> {/* JS 문법 - ||는 앞 값 truthy이면 앞 값 리턴, falsy이면 뒤 값 리턴 */} {/* (cf.) ||가 아니라 ??은 falsy가 아니라 nullish를 따진다. https://thefirstperson.tistory.com/177 */}
+      <h2>Welcome {enteredPlayerName || "unknown entity"}</h2> {/* JS 문법 - ||는 앞 값 truthy이면 앞 값 리턴, falsy이면 뒤 값 리턴 */} {/* (cf.) ||가 아니라 ??은 falsy가 아니라 nullish를 따진다. https://thefirstperson.tistory.com/177 */}
       <p>
         <input
           ref={playerName}
