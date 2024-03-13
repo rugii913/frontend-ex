25ab58ccbd3b1e344e870e07203eb4b60f43d567
rugii913 <rugii913@gmail.com>
2024-03-13 20:13:12 +0900

    (s11)refactor: useCallback 훅으로 무한 루프로부터 안전하게 만들기
    - useCallback 훅으로 무한 루프로부터 안전하게 만들기

--- s11/src/App.jsx
+++ s11/src/App.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState, useEffect } from 'react';
+import { useRef, useState, useEffect, useCallback } from 'react';
 
 import Places from './components/Places.jsx';
 import { AVAILABLE_PLACES } from './data.js';
@@ -107,7 +107,7 @@ function App() {
     }
   }
 
-  function handleRemovePlace() {
+  const handleRemovePlace = useCallback(function handleRemovePlace() {
     setPickedPlaces((prevPickedPlaces) =>
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
@@ -118,7 +118,7 @@ function App() {
       "selectedPlaces",
       JSON.stringify(storedIds.filter((id) => id !== selectedPlace.current))
     );
-  }
+  }, []);
 
   return (
     <>
