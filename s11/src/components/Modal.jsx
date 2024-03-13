84bbf374b1b9ff9d2cee83b1e40b277b1b3fc095
rugii913 <rugii913@gmail.com>
2024-03-13 17:08:08 +0900

    (s11)add: (작동 x 코드)useEffect() 사용 시도, 그러나 의존성이 제대로 추가되지 않음
    - useEffect() 사용 시도, 그러나 의존성이 제대로 추가되지 않음
      - 버튼을 눌러도 동작하지 않음

--- s11/src/components/Modal.jsx
+++ s11/src/components/Modal.jsx
@@ -1,17 +1,20 @@
-import { useRef } from 'react';
+import { useRef, useEffect } from 'react';
 import { createPortal } from 'react-dom';
 
 function Modal({ open, children }) {
   const dialog = useRef();
 
-  if (open) {
-    dialog.current.showModal();
-  } else {
-    dialog.current.close();
-  }
+  useEffect(() => {
+    if (open) { // 이 코드들도 side effect라 할 수 있다 - UI에 영향을 미치나, JSX 코드를 직접 바꾸지 않는다.
+      dialog.current.showModal();
+    } else {
+      dialog.current.close();
+    }
+  }, []) // effect의 dependency는 function component를 결국 다시 실행하도록 만드는 값이다.(prop 혹은 state 등등)
+  // 여기서는 useEffect() 내에서 open prop을 사용하고 있다. 그리고 이는 변경될 수 있다.
 
   return createPortal(
-    <dialog className="modal" ref={dialog} open={open}>
+    <dialog className="modal" ref={dialog}>
       {children}
     </dialog>,
     document.getElementById('modal')
