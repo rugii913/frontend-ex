ef1ded78f9368f20ead0d781268f928f29f552de
rugii913 <rugii913@gmail.com>
2024-03-13 20:02:10 +0900

    (s11)add: 무한 루프 위험성 있는 코드-JS 함수 객체에 의존할 때 발생하는 문제
    - 무한 루프 위험성 있는 코드
      - useEffect()에 의존성을 설정했는데, 무한 루프에 빠질 위험성이 큰 코드가 됐다.
      - 특히 JS 함수 객체에 의존할 때 발생하는 놓치기 쉬운 문제

--- s11/src/components/DeleteConfirmation.jsx
+++ s11/src/components/DeleteConfirmation.jsx
@@ -13,7 +13,22 @@ export default function DeleteConfirmation({ onConfirm, onCancel }) {
       console.log("Cleaning up timer");
       clearTimeout(timer);
     };
-  }, []);
+  }, [onConfirm]);
+  // - 여기서 의존성으로 추가된 onConfirm은 함수인데, 함수 의존성은 무한 루프를 만들 수도 있다.
+  //   - JS에서 함수는 객체이고, 따라서 여기서 App에서 onConfirm 파라미터로 넘긴 handleRemovePlace 함수는 App component가 실행될 때마다 재생성된다.
+  // - App에서의 흐름을 따라가보면
+  //   - setTimeout()에 의해 DeleteConfirmation의 onConfirm()이 실행되면 App에서 넘긴 handleRemovePlace() 실행
+  //   - handleRemovePlace() 안에서 state 업데이트 함수 실행
+  //   - App 컴포넌트 재실행
+  //   - handleRemovePlace() 함수 객체가 새로 생성
+  //   - onConfirm이 변경됨
+  //   - useEffect() 다시 실행 ... 반복
+  // - 하지만 실제로 무한 루프에 빠지진 않는데, handleRemovePlace()에서 실행되는 함수 때문에 그러함
+  //   - setModalIsOpen(false); 호출하면 Modal의 {open ? children : null} 부분 때문에 DeleteConfirmation 부분이 사라짐
+  //   - setModalIsOpen(false) 부분을 주석처리하면 바로 무한 루프를 확인할 수 있다.
+  // - 이처럼 무한 루프를 방지하기 위해서는
+  //   - 방법 1 setModalIsOpen(false)처럼 해당 요소를 DOM에서 삭제하는 코드를 추가한다.
+  //   - 방법 2 (더 안전한 방법) useCallback()을 사용한다.
 
   return (
     <div id="delete-confirmation">
